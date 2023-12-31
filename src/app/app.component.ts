7f17c8c3d95f7f93aaae3c25d0e8969aa922f0a2
harshali23 <46553270+harshali23@users.noreply.github.com>
2023-12-30 22:14:24 -0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FirstCompComponent } from './first-comp/first-comp.component';
-import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirstCompComponent, SecondCompComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'assignment7.1';
+export class AppComponent 
+{
+  title = 'assignment7.3';
 
-  public Name : string ="";
+  public Name : string ="Marvellous Infosystems";
+  status = true;
   public Fun()
-  {
-     return "Marvellous Infosystems";
-  }
- 
+    {
+        if(this.status = true)
+        {
+          this.Name = "Educating for Better Tommorow";
+        }
+    }
+  
+    public Name2 : string = "Marvellous Infosystems";
+    public Name3 : string = "Marvellous Infosystems";
+  
 }
